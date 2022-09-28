aa15f41df8b137485ddabcf28c11028013470ec7
niravp1 <nirav.padia@invitationhomes.com>
2022-09-28 19:17:01 +0530

    publish code and Airbnb
    publish code and Airbnb

--- publish_workbook.py
+++ publish_workbook.py
@@ -1,24 +1,20 @@
 import argparse
 import tableauserverclient as TSC
 import json
-
 def main(args):
-    temp_var = json.loads(args.project_id)
-
+    temp_data = json.loads(args.project_id)
     try:
         # Step 1: Sign in to server.
         tableau_auth = TSC.TableauAuth(args.username, args.password)
         server = TSC.Server(args.server_url)
         overwrite_true = TSC.Server.PublishMode.Overwrite
-        
         with server.auth.sign_in(tableau_auth):
-            for data in temp_var:
-                if data['project_path']:
+            for data in temp_data:
+                if data['file_path'] and data['project_path']:
                     # Step 2: Get all the projects on server, then look for the default one.
                     all_projects, pagination_item = server.projects.get()
                     project = next(
                         (project for project in all_projects if project.name == data['project_path']), None)
-
                     # Step 3: If default project is found, form a new workbook item and publish.
                     if project is not None:
                         new_workbook = TSC.WorkbookItem(project.id)
@@ -30,12 +26,10 @@ def main(args):
                         error = "The project could not be found."
                         raise LookupError(error)
                 else:
-                    print("Project Path is Empty.")
-                    
+                    print("File Path or Project Path is Empty.")
     except Exception as ex:
-        print('A New Exception occurred.',ex)
+        print('There was a problem.', ex)
         exit(1)
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(allow_abbrev=False)
     parser.add_argument('--username', action='store',
